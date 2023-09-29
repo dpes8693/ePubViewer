50e052a1cbcfeb619171c2fcb164bcaac586c18c
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2023-09-29 15:32:32 +0000

    Update  auto gen book example

--- script.js
+++ script.js
@@ -608,69 +608,70 @@ App.prototype.doDictionary = function (word) {
     let lmeaningsEl = ldefinitionEl.appendChild(document.createElement("div"));
     lmeaningsEl.classList.add("meanings");
     lmeaningsEl.innerHTML = "Loading";
+    lmeaningsEl.innerHTML = "這邊可以做成筆記區，可以定義有哪些功能";
 
-    fetch(`https://dict.api.pgaskin.net/word/${encodeURIComponent(word)}`).then(resp => {
-        if (resp.status >= 500) throw new Error(`Dictionary not available`);
-        if (resp.status == 404) throw new Error(`Word not found`);
-        return resp.json();
-    }).then(obj => {
-        if (obj.status == "error") throw new Error(`ApiError: ${obj.result}`);
-        return obj.result;
-    }).then(obj => {
-        console.log("dictLookup", obj);
+    // fetch(`https://dict.api.pgaskin.net/word/${encodeURIComponent(word)}`).then(resp => {
+    //     if (resp.status >= 500) throw new Error(`Dictionary not available`);
+    //     if (resp.status == 404) throw new Error(`Word not found`);
+    //     return resp.json();
+    // }).then(obj => {
+    //     if (obj.status == "error") throw new Error(`ApiError: ${obj.result}`);
+    //     return obj.result;
+    // }).then(obj => {
+    //     console.log("dictLookup", obj);
 
-        ldefinitionEl.parentElement.removeChild(ldefinitionEl);
+    //     ldefinitionEl.parentElement.removeChild(ldefinitionEl);
 
-        [obj].concat(obj.additional_words || []).concat(obj.referenced_words || []).map(word => {
-            let definitionEl = this.qs(".dictionary").appendChild(document.createElement("div"));
-            definitionEl.classList.add("definition");
+    //     [obj].concat(obj.additional_words || []).concat(obj.referenced_words || []).map(word => {
+    //         let definitionEl = this.qs(".dictionary").appendChild(document.createElement("div"));
+    //         definitionEl.classList.add("definition");
 
-            let wordEl = definitionEl.appendChild(document.createElement("div"));
-            wordEl.classList.add("word");
-            wordEl.innerText = [word.word].concat(word.alternates || []).join(", ").toLowerCase();
+    //         let wordEl = definitionEl.appendChild(document.createElement("div"));
+    //         wordEl.classList.add("word");
+    //         wordEl.innerText = [word.word].concat(word.alternates || []).join(", ").toLowerCase();
 
-            let meaningsEl = definitionEl.appendChild(document.createElement("div"));
-            meaningsEl.classList.add("meanings");
+    //         let meaningsEl = definitionEl.appendChild(document.createElement("div"));
+    //         meaningsEl.classList.add("meanings");
 
-            if (word.info && word.info.trim() != "") {
-                let infoEl = meaningsEl.appendChild(document.createElement("div"));
-                infoEl.classList.add("info");
-                infoEl.innerText = word.info;
-            }
+    //         if (word.info && word.info.trim() != "") {
+    //             let infoEl = meaningsEl.appendChild(document.createElement("div"));
+    //             infoEl.classList.add("info");
+    //             infoEl.innerText = word.info;
+    //         }
 
-            (word.meanings || []).map((meaning, i) => {
-                let meaningEl = meaningsEl.appendChild(document.createElement("div"));
-                meaningEl.classList.add("meaning");
+    //         (word.meanings || []).map((meaning, i) => {
+    //             let meaningEl = meaningsEl.appendChild(document.createElement("div"));
+    //             meaningEl.classList.add("meaning");
 
-                let meaningTextEl = meaningEl.appendChild(document.createElement("div"));
-                meaningTextEl.classList.add("text");
-                meaningTextEl.innerText = `${i + 1}. ${meaning.text}`;
+    //             let meaningTextEl = meaningEl.appendChild(document.createElement("div"));
+    //             meaningTextEl.classList.add("text");
+    //             meaningTextEl.innerText = `${i + 1}. ${meaning.text}`;
 
-                if (meaning.example && meaning.example.trim() != "") {
-                    let meaningExampleEl = meaningEl.appendChild(document.createElement("div"));
-                    meaningExampleEl.classList.add("example");
-                    meaningExampleEl.innerText = meaning.example;
-                }
-            });
+    //             if (meaning.example && meaning.example.trim() != "") {
+    //                 let meaningExampleEl = meaningEl.appendChild(document.createElement("div"));
+    //                 meaningExampleEl.classList.add("example");
+    //                 meaningExampleEl.innerText = meaning.example;
+    //             }
+    //         });
 
-            (word.notes || []).map(note => {
-                let noteEl = meaningsEl.appendChild(document.createElement("div"));
-                noteEl.classList.add("note");
-                noteEl.innerText = note;
-            });
+    //         (word.notes || []).map(note => {
+    //             let noteEl = meaningsEl.appendChild(document.createElement("div"));
+    //             noteEl.classList.add("note");
+    //             noteEl.innerText = note;
+    //         });
         
-            if (word.credit && word.credit.trim() != "") {
-                let creditEl = meaningsEl.appendChild(document.createElement("div"));
-                creditEl.classList.add("credit");
-                creditEl.innerText = word.credit;
-            }
-        });
-    }).catch(err => {
-        try {
-            console.error("dictLookup", err);
-            lmeaningsEl.innerText = err.toString();
-        } catch (err) {}
-    });
+    //         if (word.credit && word.credit.trim() != "") {
+    //             let creditEl = meaningsEl.appendChild(document.createElement("div"));
+    //             creditEl.classList.add("credit");
+    //             creditEl.innerText = word.credit;
+    //         }
+    //     });
+    // }).catch(err => {
+    //     try {
+    //         console.error("dictLookup", err);
+    //         lmeaningsEl.innerText = err.toString();
+    //     } catch (err) {}
+    // });
 };
 
 App.prototype.doFullscreen = () => {
